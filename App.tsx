9234452c4c3efec171a13eb30c5cd5aeeb3f95d8
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-05 18:50:07 +0500

    add to commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import { StyleSheet, View, Text } from "react-native";
 import React from "react";
 
+//Form Validation
+import * as Yup from 'yup'
+
+const passwordSchema = Yup.object().shape({
+  paswordLength: Yup.number()
+})
+
 
 export default function App() {
   return (
