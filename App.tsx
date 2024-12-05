ca71ecf848bd5c814e9c7383f7f30c7868e28c9c
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-05 19:00:47 +0500

    update

--- App.tsx
+++ App.tsx
@@ -6,6 +6,9 @@ import * as Yup from 'yup'
 
 const passwordSchema = Yup.object().shape({
   paswordLength: Yup.number()
+  .min(4,'Should be min of 4 characters')
+  .max(16, 'Should be max of 16 characters')
+  .required('Length is requird')
 })
 
 
