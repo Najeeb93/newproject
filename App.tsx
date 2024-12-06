534c9628973a0b37881ed76e4a735b7de9c90077
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-06 18:34:44 +0500

    update app file

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, View, Text, ScrollView, SafeAreaView } from "react-native";
 import React, { useState } from "react";
+import { Formik } from 'formik';
 
 //Form Validation
 import * as Yup from 'yup'
@@ -70,7 +71,60 @@ export default function App() {
     <ScrollView keyboardShouldPersistTaps='handled'>
       <SafeAreaView style={styles.appContainer}>
         <View style={styles.formContainer}>
-          
+          <Text style={styles.title}>Password Generator</Text>
+          <Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+           <input
+             type="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+           {errors.email && touched.email && errors.email}
+           <input
+             type="password"
+             name="password"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.password}
+           />
+           {errors.password && touched.password && errors.password}
+           <button type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </form>
+       )}
+     </Formik>
         </View>
       </SafeAreaView>
     </ScrollView>
